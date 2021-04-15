c28357e94bcca73e3d2e35c91aa0706fe4a27366
Aron Beal <aron.beal.biz@gmail.com>
2021-04-15 07:17:37 -0700

    Initial revisions
    Initial revisions to blog

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: "Aron Beal",
+      description: 'Portfoio and blog for Aron Beal',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
